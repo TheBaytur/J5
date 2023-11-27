ad11352e14ea9a6d37e9cc5ec0d6d69141d63bbb
TheBaytur <bayturcodding@gmail.com>
2023-11-27 21:40:24 +0800

    MyData

---
+++ src/MyData.java
@@ -0,0 +1,17 @@
+public class MyData {
+
+    private final int id;
+    private final String description;
+
+    public MyData(int id, String description){
+        this.id = id;
+        this.description = description;
+    }
+
+    @Override
+    public String toString(){
+        return "MyData{" +
+                "id=" + id +
+                ", decription='" + description + '\'' + '}';
+    }
+}
