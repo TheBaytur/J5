5781e80aefdbc9295fca49f366f55193dfa210b7
TheBaytur <bayturcodding@gmail.com>
2023-12-01 21:10:58 +0800

    added saveData

--- src/CachedDataSource.java
+++ src/CachedDataSource.java
@@ -1,10 +1,18 @@
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class CachedDataSource extends DataSourse{
+
+    private MyData myData;
 
     @Nullable
     @Override
     public MyData getData(){
         return null;
+    }
+
+    @Override
+    public void saveData(@NotNull MyData data){
+        myData = data;
     }
 }
--- src/DataSourse.java
+++ src/DataSourse.java
@@ -1,5 +1,9 @@
+import org.jetbrains.annotations.NotNull;
+
 public abstract class DataSourse {
 
 
     public abstract MyData getData();
+
+    public abstract void saveData(@NotNull MyData data);
 }
